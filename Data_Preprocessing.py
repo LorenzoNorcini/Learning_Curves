5b5e10d4e5f93d246f58157a7bcf970533d86273
Lorenzo Norcini <lorenzocoppinorcini77@gmail.com>
2016-08-01 19:05:56 +0200

    Fist Commit

---
+++ Data_Preprocessing.py
@@ -0,0 +1,65 @@
+from nltk.tokenize import word_tokenize
+from Categories_Data import categories
+import numpy as np
+import codecs
+import glob
+import os
+import re
+
+class Data_Preprocessor:
+
+    def tokenize(self, text):
+        tokens = word_tokenize(text)
+        alpha = [t for t in tokens if unicode(t).isalpha()]
+        return alpha
+
+    def header_not_fully_removed(self, text):
+        if ":" in text.splitlines()[0]:
+            return len(text.splitlines()[0].split(":")[0].split()) == 1
+        else:
+            return False
+
+    def strip_newsgroup_header(self, text):
+        _before, _blankline, after = text.partition('\n\n')
+        if len(after) > 0 and self.header_not_fully_removed(after):
+            after = self.strip_newsgroup_header(after)
+        return after
+
+    def strip_newsgroup_quoting(self, text):
+        _QUOTE_RE = re.compile(r'(writes in|writes:|wrote:|says:|said:'r'|^In article|^Quoted from|^\||^>)')
+        good_lines = [line for line in text.split('\n')
+            if not _QUOTE_RE.search(line)]
+        return '\n'.join(good_lines)
+
+    def strip_newsgroup_footer(self, text):
+        lines = text.strip().split('\n')
+        for line_num in range(len(lines) - 1, -1, -1):
+            line = lines[line_num]
+            if line.strip().strip('-') == '':
+                break
+        if line_num > 0:
+            return '\n'.join(lines[:line_num])
+        else:
+            return text
+
+    def raw_to_vector(self, path, to_be_stripped=["header", "footer", "quoting"], noise_threshold=-1):
+        base_dir = os.getcwd()
+        train_data = []
+        label_data = []
+        for category in categories:
+            os.chdir(base_dir)
+            os.chdir(path+"/"+category[0])
+            for filename in glob.glob("*"):
+                with codecs.open(filename, 'r', encoding='utf-8', errors='replace') as target:
+                    data = target.read()
+                    if "quoting" in to_be_stripped:
+                        data = self.strip_newsgroup_quoting(data)
+                    if "header" in to_be_stripped:
+                        data = self.strip_newsgroup_header(data)
+                    if "footer" in to_be_stripped:
+                        data = self.strip_newsgroup_footer(data)
+                    if len(data) > noise_threshold:
+                        train_data.append(data)
+                        label_data.append(category[1])
+        os.chdir(base_dir)
+        return np.array(train_data), np.array(label_data)
---
+++ main.py
@@ -0,0 +1,75 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from Data_Preprocessing import Data_Preprocessor
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.learning_curve import learning_curve
+from sklearn import cross_validation
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+full_dataset_path = "20news"
+
+dp = Data_Preprocessor()
+vectorizer = CountVectorizer(tokenizer=dp.tokenize, stop_words='english', lowercase=True, min_df=2, analyzer="word")
+clfMNB = MultinomialNB(alpha=.01)
+clfBNB = BernoulliNB(alpha=.01)
+clfPer = Perceptron(alpha=.01)
+print "Loading data..."
+start = time.time()
+data, labels = dp.raw_to_vector(full_dataset_path, ["header"])
+done = time.time()
+elapsed = done - start
+print "Elapsed: " + str(elapsed)
+start = time.time()
+print "Vectorizing..."
+data = vectorizer.fit_transform(data)
+done = time.time()
+elapsed = done - start
+print "Elapsed: " + str(elapsed)
+print "Number of Samples: " + str(data.shape[0])
+print "Number of Features: " + str(data.shape[1])
+print "Learning Curve:"
+shuffle = cross_validation.ShuffleSplit(data.shape[0], n_iter=10, random_state=0)
+
+MNB_train_size, train_scores, test_scores = \
+    learning_curve(clfMNB, data, labels, cv=shuffle, n_jobs=4, train_sizes=np.arange(0.01, 1.00, 0.01))
+
+MNB_avg_test_scores = []
+MNB_avg_train_scores = []
+for k in range(0, len(test_scores)):
+    MNB_avg_test_scores.append(np.average(test_scores[k]))
+    MNB_avg_train_scores.append(np.average(train_scores[k]))
+
+BNB_train_size, train_scores, test_scores = \
+    learning_curve(clfBNB, data, labels, cv=shuffle, n_jobs=4, train_sizes=np.arange(0.01, 1.00, 0.01))
+
+BNB_avg_test_scores = []
+BNB_avg_train_scores = []
+for k in range(0, len(test_scores)):
+    BNB_avg_test_scores.append(np.average(test_scores[k]))
+    BNB_avg_train_scores.append(np.average(train_scores[k]))
+
+Per_train_size, train_scores, test_scores = \
+    learning_curve(clfPer, data, labels, cv=shuffle, n_jobs=4, train_sizes=np.arange(0.01, 1.00, 0.01))
+
+Per_avg_test_scores = []
+Per_avg_train_scores = []
+for k in range(0, len(test_scores)):
+    Per_avg_test_scores.append(np.average(test_scores[k]))
+    Per_avg_train_scores.append(np.average(train_scores[k]))
+
+plt.plot(MNB_train_size, MNB_avg_test_scores, label="Multinomial Naive Bayes score")
+plt.plot(BNB_train_size, BNB_avg_test_scores, label="Bernoulli Naive Bayes score")
+plt.plot(Per_train_size, Per_avg_test_scores, label="Perceptron score")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1
+           , ncol=1, mode="expand", borderaxespad=0.)
+plt.axis([0, 20000, 0.1, 1])
+plt.xticks(range(0, 20000, 1000))
+plt.yticks(np.arange(0, 1, 0.1))
+for k in range(0, len(MNB_train_size), 7):
+    plt.text(MNB_train_size[k], MNB_avg_test_scores[k], str(MNB_avg_test_scores[k]))
+    plt.text(Per_train_size[k], Per_avg_test_scores[k], str(Per_avg_test_scores[k]))
+    plt.text(BNB_train_size[k], BNB_avg_test_scores[k], str(BNB_avg_test_scores[k]))
+plt.show()
